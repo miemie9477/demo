6dd2217630865082a4ff47c401de970c5cd2d687
miemie9477 <jelena0625@gmail.com>
2024-04-01 23:07:32 +0800

    first push of demo

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Test from "./tsx/Test"
+
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <Test />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/tsx/Test.tsx
@@ -0,0 +1,5 @@
+function Test() {
+  return <h1>hello tsx!!</h1>;
+}
+
+export default Test
